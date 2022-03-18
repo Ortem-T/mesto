e0684e5ee48eae2f0ae0f936250c3e17afb28dc4
Artem Tarasov <a.tarasoff.a@yandex.ru>
2022-03-18 20:01:32 +0300

    feat: подключить js

---
+++ index.js
@@ -0,0 +1,42 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup');
+const closeButton = popupElement.querySelector('.popup__close');
+const ESC_KEY = "Escape";
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__input_name');
+const aboutMeInput = formElement.querySelector('.form__input_about-me');
+const profileName = document.querySelector('.profile__name');
+const profileAboutMe = document.querySelector('.profile__about-me'); 
+
+function openPopup() {
+    popupElement.classList.add('popup_opened');
+    document.addEventListener('keyup', onDocumentKeyUp)
+}
+
+function closePopup() {
+    popupElement.classList.remove('popup_opened');
+    document.removeEventListener('keyup', onDocumentKeyUp)
+}
+
+function onDocumentKeyUp(event){
+    if (event.key === ESC_KEY) {
+        closePopup();
+    }
+}
+
+editButton.addEventListener('click', (event) => {
+    openPopup()
+});
+
+closeButton.addEventListener('click', closePopup)
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileAboutMe.textContent = aboutMeInput.value;
+    closePopup();
+
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
