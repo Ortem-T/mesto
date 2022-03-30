53af91d760eaeb77ba92f20bd125218edaed1bd3
Artem Tarasov <a.tarasoff.a@yandex.ru>
2022-03-30 15:35:33 +0300

    feat: create popup adding a photo

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,58 @@
 const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
 const popupElement = document.querySelector('.popup');
-const closeButton = popupElement.querySelector('.popup__close');
+const closeButtons = document.querySelectorAll('.popup__close');
 const ESC_KEY = "Escape";
 const formElement = document.querySelector('.form');
 const nameInput = formElement.querySelector('.form__input_type_name');
 const aboutMeInput = formElement.querySelector('.form__input_type_about-me');
 const profileName = document.querySelector('.profile__name');
 const profileAboutMe = document.querySelector('.profile__about-me'); 
+const popupProfile = document.querySelector('.popup_type_profile');
+const popupImg = document.querySelector('.popup_type_img');
 
-function openPopup() {
+const initialCards = [
+    {
+      name: 'Подольск',
+      link: './images/podolsk.jpg'
+    },
+    {
+      name: 'Рускеала',
+      link: './images/ruskeala.jpg'
+    },
+    {
+      name: 'Гусиноозерск',
+      link: './images/gusinoozersk.jpg'
+    },
+    {
+      name: 'Южно-Сахалинск',
+      link: './images/yuzhno-sakhalinsk.jpg'
+    },
+    {
+      name: 'Солнечнодольск',
+      link: './images/solnechnodolsk.jpg'
+    },
+    {
+      name: 'Тверь',
+      link: './images/tver.jpg'
+    }
+  ];
+
+function openPopup(popupElement) {
     popupElement.classList.add('popup_opened');
     document.addEventListener('keyup', onDocumentKeyUp)
+}
+
+function editProfile() {
+    openPopup(popupProfile)
+
     nameInput.value = profileName.textContent;
     aboutMeInput.value = profileAboutMe.textContent;
 }
 
 function closePopup() {
-    popupElement.classList.remove('popup_opened');
+    const currentPopup = document.querySelector('.popup_opened');
+    currentPopup.classList.remove('popup_opened');
     document.removeEventListener('keyup', onDocumentKeyUp)
 }
 
@@ -27,10 +63,17 @@ function onDocumentKeyUp(event){
 }
 
 editButton.addEventListener('click', (event) => {
-    openPopup()
+    editProfile()
 });
 
-closeButton.addEventListener('click', closePopup)
+addButton.addEventListener('click', (event) => {
+    openPopup(popupImg)
+});
+
+
+for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', closePopup);
+}
 
 function handleProfileFormSubmit (evt) {
     evt.preventDefault();
@@ -38,7 +81,6 @@ function handleProfileFormSubmit (evt) {
     profileName.textContent = nameInput.value;
     profileAboutMe.textContent = aboutMeInput.value;
     closePopup();
-
 }
 
 formElement.addEventListener('submit', handleProfileFormSubmit);
